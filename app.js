73d3c2401c30f2689224ba4178ac6d9bcc0d8fce
asmithresearchtest <32855632+asmithresearchtest@users.noreply.github.com>
2017-10-17 16:34:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World - happy Friday!')
 })
 
 if (process.env.NODE_ENV !== 'test') {
