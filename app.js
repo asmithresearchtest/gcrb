c389ba68ddf2d40613ed56e30ff43aa277abda12
asmithresearchtest <32855632+asmithresearchtest@users.noreply.github.com>
2017-10-30 13:02:13 -0700

    Update app.js
    Test build trigge

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World modified!')
 })
 
 if (process.env.NODE_ENV !== 'test') {
